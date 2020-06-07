bf121ea62baaf2a2917f43a545485c8af0a2f57a
steHawk <karmamodi321@gmail.com>
2020-06-07 17:59:26 +0530

    sideNav Links

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -12,15 +12,16 @@ class Navbar extends Component {
     auth: PropTypes.object.isRequired,
     logout: PropTypes.func.isRequired,
   };
+
+
+
   render() {
     const { isAuthenticated } = this.props.auth;
     function handleBlur(e) {
       document.querySelector('.main-menu').classList.toggle('show');
     }
-
+   
     return (
-
-
       <div className="main-nav">
         <ul className="main-menu" >
           <li>   <div onClick={handleBlur}><div class="close-i">
@@ -28,29 +29,29 @@ class Navbar extends Component {
             <i class="fa fa-times fa-3x" aria-hidden="true"></i>
           </div>   </div>
           </li>
-          <li><a href="/doctors">Doctor Consultation</a></li>
-          <li> <a href="/alltests">Lab Services</a></li>
-          <li> <a href="/medicine"> Medicines and Medical Supplies</a></li>
+          <li><Link to="/doctors" onClick={handleBlur}>Doctor Consultation</Link></li>
+          <li> <Link to="/alltests" onClick={handleBlur}>Lab Services</Link></li>
+          <li> <Link href="/medicine" onClick={handleBlur}> Medicines and Medical Supplies</Link></li>
 
           {isAuthenticated ? (
             <div className="right">
-              <li> <Link to="/profile"><i className="fa fa-user-circle-o fa-2x" aria-hidden="true"></i>
+              <li> <Link to="/profile" onClick={handleBlur}><i className="fa fa-user-circle-o fa-2x" aria-hidden="true"></i>
                            Profile</Link></li>
-              <li> <Link to="/" onClick={this.props.logout}><i className="fa fa-sign-in fa-2x" aria-hidden="true"></i>
+              <li> <Link to="/" onClick={() => { handleBlur(); this.props.logout()}}><i className="fa fa-sign-in fa-2x" aria-hidden="true"></i>
                            Logout</Link>
               </li>
-              <li> <Link to="/cart"><i className="fa fa-shopping-cart fa-2x" aria-hidden="true"></i>
+              <li> <Link to="/cart" onClick={handleBlur}><i className="fa fa-shopping-cart fa-2x" aria-hidden="true"></i>
                            Cart</Link></li>
             </div>
 
-          ) : (
+          ) : ( 
               <div className="right">
-                <li> <Link to="/login"><i className="fa fa-user-circle-o fa-2x" aria-hidden="true"></i>
+                <li> <Link to="/login" onClick={handleBlur}><i className="fa fa-user-circle-o fa-2x" aria-hidden="true"></i>
                           Profile</Link></li>
-                <li> <Link to="/login"><i className="fa fa-sign-in fa-2x" aria-hidden="true"></i>
+                <li> <Link to="/login" onClick={handleBlur}><i className="fa fa-sign-in fa-2x" aria-hidden="true"></i>
                           Login</Link>
                 </li>
-                <li> <Link to="/login"><i className="fa fa-shopping-cart fa-2x" aria-hidden="true"></i>
+                <li> <Link to="/login" onClick={handleBlur}><i className="fa fa-shopping-cart fa-2x" aria-hidden="true"></i>
                           Cart</Link></li>
               </div>
 
@@ -67,4 +68,4 @@ class Navbar extends Component {
 const mapStateToProps = (state) => ({
   auth: state.auth,
 });
-export default connect(mapStateToProps, {logout})(Navbar);
+export default connect(mapStateToProps, { logout })(Navbar);
