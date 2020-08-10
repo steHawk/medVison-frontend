f85cde97ef972cf742ed4b968b7088f6b3b24e9a
Himanshusinghofficial <hs882677@gmail.com>
2020-08-10 16:14:24 +0530

    webpack change

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,51 @@
-// module.exports = {
-//   module: {
-//     rules: [
-//       {
-//         test: /\.js$/,
-//         use: "babel-loader",
-//       },
-//       {
-//         test: /\.css$/,
-//         use: ["style-loader", "css-loader"],
-//       },
-//       {
-//         test: /\.(png|j?g|svg|gif)?$/,
-//         use: "file-loader",
-//       },
-//     ],
-//   },
-// };
-
-var path = require('path');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-
 module.exports = {
-  entry: './app/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'index_bundle.js',
     publicPath: '/'
   },
   module: {
     rules: [
-      { test: /\.(js)$/, use: 'babel-loader' },
-      { test: /\.css$/, use: ['style-loader', 'css-loader'] }
-    ]
+      {
+        test: /\.js$/,
+        use: "babel-loader",
+      },
+
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|j?g|svg|gif)?$/,
+        use: "file-loader",
+      },
+    ],
   },
   devServer: {
     historyApiFallback: true,
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'app/index.html'
-    })
-  ]
 };
+
+// var path = require('path');
+// var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// module.exports = {
+//   entry: './app/index.js',
+//   output: {
+//     path: path.resolve(__dirname, 'dist'),
+//     filename: 'index_bundle.js',
+//     publicPath: '/'
+//   },
+//   module: {
+//     rules: [
+//       { test: /\.(js)$/, use: 'babel-loader' },
+//       { test: /\.css$/, use: ['style-loader', 'css-loader'] }
+//     ]
+//   },
+//   devServer: {
+//     historyApiFallback: true,
+//   },
+//   plugins: [
+//     new HtmlWebpackPlugin({
+//       template: 'app/index.html'
+//     })
+//   ]
+// };
