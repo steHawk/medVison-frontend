4f9cdef36a17a1880df7c82034b428399671d765
Himanshusinghofficial <hs882677@gmail.com>
2020-08-11 13:44:09 +0530

    add .htaccess

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,55 @@
-module.exports = {
-  output: {
-    publicPath: '/'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        use: "babel-loader",
-      },
-
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.(png|j?g|svg|gif)?$/,
-        use: "file-loader",
-      },
-    ],
-  },
-  devServer: {
-    historyApiFallback: true,
-  },
-};
-
-// var path = require('path');
-// var HtmlWebpackPlugin = require('html-webpack-plugin');
-
 // module.exports = {
-//   entry: './app/index.js',
 //   output: {
-//     path: path.resolve(__dirname, 'dist'),
-//     filename: 'index_bundle.js',
 //     publicPath: '/'
 //   },
 //   module: {
 //     rules: [
-//       { test: /\.(js)$/, use: 'babel-loader' },
-//       { test: /\.css$/, use: ['style-loader', 'css-loader'] }
-//     ]
+//       {
+//         test: /\.js$/,
+//         use: "babel-loader",
+//       },
+
+//       {
+//         test: /\.css$/,
+//         use: ["style-loader", "css-loader"],
+//       },
+//       {
+//         test: /\.(png|j?g|svg|gif)?$/,
+//         use: "file-loader",
+//       },
+//     ],
 //   },
 //   devServer: {
 //     historyApiFallback: true,
+//     contentBase: './',
+//     hot: true
 //   },
-//   plugins: [
-//     new HtmlWebpackPlugin({
-//       template: 'app/index.html'
-//     })
-//   ]
 // };
+
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './app/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      { test: /\.(js)$/, use: 'babel-loader' },
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] }
+    ]
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './',
+    hot: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'app/index.html'
+    })
+  ]
+};
